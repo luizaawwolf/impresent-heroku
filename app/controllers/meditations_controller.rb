a3b8ff7a1b732055e4f200c368a109f5fa00500c
Jessica Su <js803@duke.edu>
2020-01-11 16:49:41 -0500

    adding download to CSV option

--- app/controllers/meditations_controller.rb
+++ app/controllers/meditations_controller.rb
@@ -1,3 +1,7 @@
+require 'net/http'
+require 'json'
+require 'uri'
+
 class MeditationsController < ApplicationController
 skip_before_action :verify_authenticity_token
 	def new
@@ -20,23 +24,47 @@ skip_before_action :verify_authenticity_token
 	def index
 		@meditations = Meditation.all 
 
+		
 		respond_to do |format|
 	      format.html
 	      format.js
+	      format.csv {send_data @meditations.to_csv, filename: "meditationData.csv"}
 	      format.json do 
 	      	render json: @meditations.to_json
 	      end
+
 	    end
 	end
 
 	def edit
+		@meditation = Meditation.find(params[:id])
 	end
 
 	def show
+		@meditation = Meditation.find(params[:id])
 	end
 
 	def destroy
 		@meditation = Meditation.find(params[:id])
 		@meditation.destroy
 	end
+
+	def to_csv(options = {})
+		desired_columns = ["Participant", "Date/Time", "Emotions", "Meditation", "Stress Rating Before", "Stress Rating After"]
+		CSV.generate(options) do |csv|
+			csv << desired_columns 
+			@meditations.each  do |meditation|
+				user_name = meditation.user 
+				date_time = meditation.datetime
+				emotions = meditation.emotions 
+				med = meditation.meditation
+				stressBefore = meditation.stresslevelbefore 
+				stressAfter = medtiation.stresslevelafter 
+
+				csv << [user_name, date_time, emotions, med, stressBefore, stressAfter ]
+			end
+
+
+		end 
+    end
 end
--- app/models/meditation.rb
+++ app/models/meditation.rb
@@ -1,2 +1,24 @@
+require 'csv'
+
 class Meditation < ApplicationRecord
+	
+	def self.to_csv(options = {})
+		desired_columns = ["Participant", "Date/Time", "Emotions", "Meditation", "Stress Rating Before", "Stress Rating After"]
+		CSV.generate(options) do |csv|
+			csv << desired_columns 
+
+			all.each  do |meditation|
+				user_name = meditation.user 
+				date_time = meditation.datetime
+				emotions = meditation.emotions 
+				med = meditation.meditation
+				stressBefore = meditation.stresslevelbefore 
+				stressAfter = meditation.stresslevelafter 
+
+				csv << [user_name, date_time, emotions, med, stressBefore, stressAfter ]
+			end
+
+
+		end 
+    end
 end
