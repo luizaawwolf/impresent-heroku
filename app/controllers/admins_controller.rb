7b006038628519fa0c33483742a38520dd22c351
Luiza <annaluza.wolf@duke.edu>
2020-06-20 17:29:00 -0400

    this is working

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -6,8 +6,9 @@ class AdminsController < ApplicationController
   end
 
   def create
-   permitted_admins = ["stephanie.schuette@duke.edu"]
-   if params[:admin][:username].in?(permitted_admins)
+   permitted_admins = ["stephanie.schuette@duke.edu","app"]
+   @admin = Admin.find_by_username(params[:admin][:username])
+   if params[:admin][:username].in?(permitted_admins) && @admin==nil
      @admin = Admin.create(params.require(:admin).permit(:username,        
      :password))
      session[:admin_id] = @admin.id
